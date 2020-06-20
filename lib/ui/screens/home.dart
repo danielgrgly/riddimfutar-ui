7f0df45f946059a64f2f6d54a8632086bfc722a3
danielgrgly <danielgrgly@gmail.com>
2020-06-20 12:23:22 +0200

    add assets and launcher icons

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -2,18 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 final String assetName = 'assets/svg/logo.svg';
-final Widget logo =
-    SvgPicture.asset(assetName, semanticsLabel: 'RIDDIMFUTÁR logo');
+final Widget svgIcon = SvgPicture.asset(assetName,
+    color: Colors.red, semanticsLabel: 'A red up arrow');
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: logo,
-      ),
       body: Column(
-        children: <Widget>[Text("Hey hi hello!")],
+        children: <Widget>[svgIcon, Text("Hey hi hello!")],
       ),
     );
   }
