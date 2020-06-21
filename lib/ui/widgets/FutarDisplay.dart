45933f7843044f1c341b44472104535de5fe74db
danielgrgly <danielgrgly@gmail.com>
2020-06-21 15:14:27 +0200

    refact, add rotating visualizer

---
+++ lib/ui/widgets/FutarDisplay.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import "../../core/models/TripDetails.dart";
+import '../../core/models/Vehicle.dart';
+
+class FutarDisplay extends StatelessWidget {
+  FutarDisplay({this.trip});
+
+  final TripDetails trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).systemGestureInsets.top + 188,
+          color: Color.fromRGBO(70, 71, 112, 1),
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top + 12,
+              left: 24.0,
+              right: 24.0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 34,
+                      height: 34,
+                      child: SvgPicture.asset(
+                        iconPath(trip.type),
+                        semanticsLabel: 'vehicle icon',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Container(
+                      width: 68,
+                      height: 34,
+                      child: Center(
+                        child: Text(
+                          trip.name,
+                          style: TextStyle(
+                            color: trip.type == VehicleType.TRAM
+                                ? Color.fromRGBO(43, 41, 41, 1)
+                                : Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: trip.color,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 30,
+                      height: 84,
+                      child: SvgPicture.asset(
+                        "assets/svg/terminus.svg",
+                        semanticsLabel: 'terminus illustration',
+                        color: trip.color,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: Text(
+                        trip.description,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                width: 30,
+                height: 72,
+                child: Stack(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 30,
+                      height: 72,
+                      child: SvgPicture.asset(
+                        "assets/svg/stop.svg",
+                        semanticsLabel: 'stop illustration',
+                        color: trip.color,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 6, top: 48),
+                      child: SizedBox(
+                        width: 18,
+                        height: 18,
+                        child: SvgPicture.asset(
+                          "assets/svg/dot.svg",
+                          semanticsLabel: 'dot for stop illustration',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Text(
+                    trip.stops[trip.stopSequence].name,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/widgets/Visualizer.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import "../../core/models/TripDetails.dart";
+import '../../core/models/Vehicle.dart';
+
+class Visualizer extends StatefulWidget {
+  Visualizer({this.trip});
+
+  final TripDetails trip;
+
+  @override
+  _VisualizerState createState() => _VisualizerState();
+}
+
+class _VisualizerState extends State<Visualizer> with TickerProviderStateMixin {
+  double _width = 150;
+  double _height = 150;
+  double _rotation = 0;
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 4000),
+      vsync: this,
+    );
+
+    kickoffRotate();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void kickoffRotate() async {
+    _controller.forward(from: 0);
+
+    while (true) {
+      // You have to provide a condition to know when to stop
+      await new Future.delayed(const Duration(seconds: 4), () {
+        _controller.forward(from: 0);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 10),
+        curve: Curves.linear,
+        width: _width,
+        height: _height,
+        child: RotationTransition(
+          turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+          child: SvgPicture.asset(
+            iconPath(widget.trip.type),
+            semanticsLabel: 'visualizer',
+          ),
+        ),
+      ),
+    );
+  }
+}
