f96c7ffbd50f17714afb9f284b3a59708162b27a
danielgrgly <danielgrgly@gmail.com>
2020-06-28 12:29:46 +0200

    fix the soundservice

--- lib/core/services/SoundService.dart
+++ lib/core/services/SoundService.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:async';
-// import 'dart:math' show cos, sqrt, asin;
+import 'dart:math' show cos, sqrt, asin;
 
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -16,24 +16,21 @@ final AudioPlayer _mainPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
 final Throttling _thr5 = new Throttling(duration: Duration(seconds: 5));
 final Location _location = new Location();
 
-Future<dynamic> fetchPercent(String id) async {
-  final response = await http.get(
-    'http://localhost:8080/api/v1/percents/$id',
-  );
-
-  if (response.statusCode == 200) {
-    return json.decode(response.body);
-  } else {
-    throw Exception('Failed to load vehicle percentage');
-  }
+double calculateDistance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;
+  var c = cos;
+  var a = 0.5 -
+      c((lat2 - lat1) * p) / 2 +
+      c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+  return 12742 * asin(sqrt(a));
 }
 
 class SoundService {
   TripDetails tripData;
   MusicDetails musicData;
-  List<int> breakpoints;
   List<String> nextQueue;
   String tripId;
+  int sequence;
   Function updateStop;
   Function endTrip;
 
@@ -45,6 +42,7 @@ class SoundService {
   ) {
     this.tripId = tripId;
     this.tripData = trip;
+    this.sequence = trip.stopSequence;
     this.nextQueue = new List<String>();
     this.updateStop = updateStop;
     this.endTrip = endTrip;
@@ -58,16 +56,14 @@ class SoundService {
     // welcome onboard!
     nextQueue.add("https://storage.googleapis.com/futar/EF-udv.mp3");
 
-    final data = await fetchPercent(this.tripId);
-    final sequence = data["stopSequence"];
-    _reachBreakpoint(0, sequence);
+    _reachBreakpoint(0);
 
     _listenSounds();
 
-    _timer = Timer.periodic(
-      Duration(seconds: 5),
-      (Timer t) => _checkBreakpoint(),
-    );
+    // _timer = Timer.periodic(
+    //   Duration(seconds: 5),
+    //   (Timer t) => _checkBreakpoint(),
+    // );
 
     // _location.onLocationChanged.listen((LocationData location) async {
     //   _thr5.throttle(() async {
@@ -75,32 +71,42 @@ class SoundService {
     //   });
     // });
 
+    int percent = 0;
+
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      percent += 2;
+
+      if (percent >= 100) {
+        percent = 0;
+      }
+
+      _checkBreakpoint(percent);
+    });
+
     _mainPlayer.onPlayerCompletion.listen((event) {
       _listenSounds();
     });
   }
 
-  void _checkBreakpoint() async {
-    final data = await fetchPercent(this.tripId);
-    final percent = data["stopDistancePercent"];
-    final sequence = data["stopSequence"];
-
-    if (percent >= breakpoints[0]) {
-      _reachBreakpoint(percent, sequence);
+  void _checkBreakpoint(int percent) async {
+    if (percent >= musicData.files[0].breakpoint) {
+      _reachBreakpoint(percent);
+    }
+    if (percent >= 97) {
+      _reachBreakpoint(100);
     }
   }
 
-  void _reachBreakpoint(int percent, int sequence) {
-    print("percent: $percent");
-    final index = breakpoints.lastIndexWhere((element) => element <= percent);
-    final MusicFile music = musicData.files[index];
+  void _reachBreakpoint(int percent) async {
+    final MusicFile music = musicData.files[0];
     final musicFile =
         "https://storage.googleapis.com/riddim/riddim/0/" + music.fileName;
     final String stopFile = "https://storage.googleapis.com/futar/" +
         this.tripData.stops[sequence].fileName;
 
+    musicData.files.removeAt(0);
+
     if (percent == 0) {
-      print("0");
       nextQueue.add("https://storage.googleapis.com/futar/EF-kov.mp3");
       // stop name
       nextQueue.add(stopFile);
@@ -109,65 +115,63 @@ class SoundService {
       }
       // music file
       nextQueue.add(musicFile);
-    } else if (percent == 100) {
-      print("100");
+    } else if (percent >= 97) {
       // stop name
       nextQueue.add(stopFile);
       // music file
       nextQueue.add(musicFile);
 
-      print(tripData.stops.length);
-      print(sequence);
       if (tripData.stops.length - 1 >= sequence + 1) {
-        print("aight sequence");
-        _fetchMusic();
-        _reachBreakpoint(0, sequence + 1);
-        updateStop(sequence + 1);
+        sequence++;
+        await _fetchMusic();
+        _reachBreakpoint(0);
       } else {
         nextQueue.add("https://storage.googleapis.com/futar/EF-veg.mp3");
         nextQueue.add("https://storage.googleapis.com/futar/EF-visz.mp3");
-        print("fuck u imma end");
       }
     } else {
-      print("nothing special");
       nextQueue.add(musicFile);
     }
 
-    if (!music.loopable) {
-      breakpoints.removeAt(index);
-      musicData.files.removeAt(index);
+    // if (!music.loopable) {
+    //   // breakpoints.removeAt(index);
+    //   // musicData.files.removeAt(index);
 
-      // if (musicData.files.length == index + 1) {
-      //   // the next stop!
-      //   if (sequence + 1 >= tripData.stops.length) {
-      //     // no next stop - end of trip
-      //     // this.endTrip();
-      //   } else {
-      //     // next stop exists
-      //     this._fetchMusic();
-      //     this._reachBreakpoint(0, sequence + 1);
-      //     updateStop;
-      //   }
-      // } else {
-      //   // next file
-      final MusicFile nextMusic = musicData.files[index + 1];
-      final nextFile = "https://storage.googleapis.com/riddim/riddim/0/" +
-          nextMusic.fileName;
-      nextQueue.add(nextFile);
-      // }
-    }
+    //   // if (musicData.files.length == index + 1) {
+    //   //   // the next stop!
+    //   //   if (sequence + 1 >= tripData.stops.length) {
+    //   //     // no next stop - end of trip
+    //   //     // this.endTrip();
+    //   //   } else {
+    //   //     // next stop exists
+    //   //     this._fetchMusic();
+    //   //     this._reachBreakpoint(0, sequence + 1);
+    //   //     updateStop;
+    //   //   }
+    //   // } else {
+    //   //   // next file
+    //   final MusicFile nextMusic = musicData.files[index + 1];
+    //   final nextFile = "https://storage.googleapis.com/riddim/riddim/0/" +
+    //       nextMusic.fileName;
+    //   nextQueue.add(nextFile);
+    //   // }
+    // }
   }
 
   void _listenSounds() {
+    if (nextQueue.length > 1) {
+      nextQueue.removeAt(0);
+    }
+
     _play(nextQueue[0]);
+
+    if (nextQueue[0] == "https://storage.googleapis.com/futar/EF-kov.mp3") {
+      updateStop(sequence);
+    }
 
     if (nextQueue[0] == "https://storage.googleapis.com/futar/EF-visz.mp3") {
       nextQueue = [];
       endTrip();
-    }
-
-    if (nextQueue.length > 1) {
-      nextQueue.removeAt(0);
     }
   }
 
@@ -178,8 +182,6 @@ class SoundService {
 
     if (response.statusCode == 200) {
       this.musicData = MusicDetails.fromJson(json.decode(response.body));
-      this.breakpoints =
-          musicData.files.map((MusicFile file) => file.breakpoint).toList();
     } else {
       throw Exception('Failed to load vehicle percentage');
     }
